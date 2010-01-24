7825ffce05b8091c0d47264380ab0042b60c6598
pjf <pjf@aaf4ce65-038a-4fed-a244-67e203654444>
2010-01-24 18:27:21 +0000

    First 0.1

--- lib/Class/Usul/Base.pm
+++ lib/Class/Usul/Base.pm
@@ -4,7 +4,7 @@ package Class::Usul::Base;
 
 use strict;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.2.%d', q$Rev$ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev$ =~ /\d+/gmx );
 
 use Class::Usul::Constants;
 use Class::Usul::Time;
@@ -297,7 +297,7 @@ Class::Usul::Base - Base class utility methods
 
 =head1 Version
 
-0.2.$Revision$
+0.1.$Revision$
 
 =head1 Synopsis
 
