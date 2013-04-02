001c3fb06de66a5bf1d0108aec22075232161d4c
pjf <pjf@aaf4ce65-038a-4fed-a244-67e203654444>
2013-04-02 14:39:40 +0000

    Updated for inc::Bob 1.9

--- inc/SubClass.pm
+++ inc/SubClass.pm
@@ -1,10 +1,12 @@
-# @(#)$Id$
-# Bob-Version: 1.7
+# @(#)Ident: SubClass.pm 2013-04-02 14:37 pjf ;
+# Bob-Version: 1.9
 
 use Pod::Select;
 
 sub ACTION_distmeta {
    my $self = shift;
+
+   $self->notes->{create_readme_md} and $self->_create_readme_md();
 
    $self->notes->{create_readme_pod} and podselect( {
       -output => q(README.pod) }, $self->dist_version_from );
@@ -24,3 +26,18 @@ sub create_build_script {
    return;
 }
 
+# Private methods
+
+sub _create_readme_md {
+   print "Creating README.md using Pod::Markdown\n"; require Pod::Markdown;
+
+   my $self   = shift;
+   my $parser = Pod::Markdown->new;
+   my $path   = $self->dist_version_from;
+
+   open my $in,  '<', $path       or die "Path ${path} cannot open: ${!}";
+   $parser->parse_from_filehandle( $in ); close $in;
+   open my $out, '>', 'README.md' or die "File README.md cannot open: ${!}";
+   print {$out} $parser->as_markdown; close $out;
+   return;
+}
