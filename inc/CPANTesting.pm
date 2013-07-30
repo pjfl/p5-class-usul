1af7ef315788b9fbcb0022faeea5592ed63e0bd5
pjfl <github@roxsoft.co.uk>
2013-07-30 14:27:50 +0100

    0.22.16 New method for CPANTesting min Perl ver

--- inc/CPANTesting.pm
+++ inc/CPANTesting.pm
@@ -1,4 +1,4 @@
-# @(#)$Ident: CPANTesting.pm 2013-07-30 08:25 pjf ;
+# @(#)Ident: CPANTesting.pm 2013-07-30 13:25 pjf ;
 
 package CPANTesting;
 
@@ -15,24 +15,19 @@ sub should_abort {
    is_testing() or return 0;
 
    $host eq q(xphvmfred) and return
-      "Terminated Stauner ${host} - cc06993e-a5e9-11e2-83b7-87183f85d660";
-   # df276fba-f57c-11e2-8c80-50d7c5c10595 - no words
-   # Chris Williams - Put my pause id in your stop list
-   if ($host =~ m{ bingosnet }mx) { sleep 10 while 1; }
-   if ($host =~ m{ fremen    }mx) { sleep 10 while 1; }
-   if ($host =~ m{ frogman   }mx) { sleep 10 while 1; }
-
+      "ABORT: ${host} - cc06993e-a5e9-11e2-83b7-87183f85d660";
    return 0;
 }
 
 sub test_exceptions {
-   my $p = shift; is_testing() or return 0;
+   my $p = shift; my $perl_ver = $p->{requires}->{perl};
 
-   $p->{stop_tests}              and return 'CPAN Testing stopped in Build.PL';
-   $osname eq q(mirbsd)          and return 'Mirbsd OS unsupported';
-   $host   =~ m{ nigelhorne }msx and return
-      "Stopped Horne ${host} - irrelevant Perl versions";
+   is_testing()         or  return 0;
+   $] < $perl_ver       and return "TESTS: Perl minimum ${perl_ver}";
+   $p->{stop_tests}     and return 'TESTS: CPAN Testing stopped in Build.PL';
+   $osname eq q(mirbsd) and return 'TESTS: Mirbsd OS unsupported';
 
+   $host =~ m{ nigelhorne }msx and return "TESTS: Irrelevant Perl versions";
    return 0;
 }
 
--- lib/Class/Usul.pm
+++ lib/Class/Usul.pm
@@ -1,10 +1,10 @@
-# @(#)$Ident: Usul.pm 2013-07-30 10:20 pjf ;
+# @(#)$Ident: Usul.pm 2013-07-30 13:27 pjf ;
 
 package Class::Usul;
 
 use 5.010001;
 use namespace::sweep;
-use version; our $VERSION = qv( sprintf '0.22.%d', q$Rev: 15 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.22.%d', q$Rev: 16 $ =~ /\d+/gmx );
 
 use Class::Usul::Constants;
 use Class::Usul::Functions  qw( data_dumper merge_attributes throw );
@@ -109,7 +109,7 @@ Class::Usul - A base class providing config, locking, logging, and l10n
 
 =head1 Version
 
-Describes Class::Usul version v0.22.$Rev: 15 $
+Describes Class::Usul version v0.22.$Rev: 16 $
 
 =head1 Synopsis
 
