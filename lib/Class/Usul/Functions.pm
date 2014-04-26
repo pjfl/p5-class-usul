f63324287f3aea8456ad330999adf78eba9b44f2
pjfl <github@roxsoft.co.uk>
2014-04-26 13:42:42 +0100

    0.39 Doc update

--- lib/Class/Usul/Functions.pm
+++ lib/Class/Usul/Functions.pm
@@ -912,7 +912,19 @@ class L<CatalystX::Usul::Exception>. Returns a new error object
 
    $directory_path = find_apphome $appclass, $homedir, $extns
 
-Returns the path to the applications home directory
+Returns the path to the applications home directory. Searches the following:
+
+   # 0.  Undef appclass and pass the directory in (short circuit the search)
+   # 1a. Environment variable - for application directory
+   # 1b. Environment variable - for config file
+   # 2a. Users home directory - dot file containing shell env variable
+   # 2b. Users home directory - dot directory is apphome
+   # 3.  Well known path containing shell env file
+   # 4.  Default install prefix
+   # 5a. Config file found in @INC - underscore as separator
+   # 5b. Config file found in @INC - dash as separator
+   # 6.  Pass the default in
+   # 7.  Default to /tmp
 
 =head2 find_source
 
