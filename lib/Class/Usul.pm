670928c8464a3f40f4a074f24a9e9cef09f5a471
pjfl <github@roxsoft.co.uk>
2015-03-14 20:07:53 +0000

    0.57 Released

--- lib/Class/Usul.pm
+++ lib/Class/Usul.pm
@@ -3,7 +3,7 @@ package Class::Usul;
 use 5.010001;
 use feature 'state';
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.56.%d', q$Rev: 6 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.57.%d', q$Rev: 1 $ =~ /\d+/gmx );
 
 use Moo;
 use Class::Usul::Constants  qw( EXCEPTION_CLASS FALSE TRUE );
@@ -69,7 +69,7 @@ Class::Usul - A base class providing config, locking, logging, and l10n
 
 =head1 Version
 
-Describes Class::Usul version v0.56.$Rev: 6 $
+Describes Class::Usul version v0.57.$Rev: 1 $
 
 =head1 Synopsis
 
