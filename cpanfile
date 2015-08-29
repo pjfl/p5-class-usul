6eeaefef6ac299743ec5fca6eda88c94410b7d9e
pjfl <github@roxsoft.co.uk>
2015-08-29 11:06:52 +0100

    Use BuildRequires not TestRequires for Kwalitee

--- cpanfile
+++ cpanfile
@@ -35,15 +35,23 @@ recommends "Proc::ProcessTable" => "0.42";
 
 on 'build' => sub {
   requires "Module::Build" => "0.4004";
+  requires "version" => "0.88";
 };
 
 on 'test' => sub {
   requires "Capture::Tiny" => "0.22";
+  requires "File::Spec" => "0";
   requires "Hash::MoreUtils" => "0.05";
   requires "Module::Build" => "0.4004";
+  requires "Module::Metadata" => "0";
+  requires "Sys::Hostname" => "0";
   requires "Test::Deep" => "0.108";
   requires "Test::Requires" => "0.06";
   requires "version" => "0.88";
+};
+
+on 'test' => sub {
+  recommends "CPAN::Meta" => "2.120900";
 };
 
 on 'configure' => sub {
--- t/boilerplate.pm
+++ t/boilerplate.pm
@@ -20,11 +20,10 @@ BEGIN {
    $perl_ver = $notes->{min_perl_version} || 5.008;
 
    if ($notes->{testing}) {
-      $Bin  =~ m{ : .+ : }mx and plan skip_all => 'Two colons in $Bin path';
-      $host =~ m{ \A davids-macbook }mx and
-         plan skip_all => 'Broken smoker 158a80fe-0a22-11e5-9d49-e60ed23c8333';
-      # TODO: Remove this when service is restored
-      plan skip_all => 'CPAN Testing down Aug 2015';
+      $Bin  =~ m{ : .+ : }mx and plan
+         skip_all => 'Two colons in $Bin path';
+      $host =~ m{ \A davids-macbook }mx and plan
+         skip_all => 'Broken smoker 158a80fe-0a22-11e5-9d49-e60ed23c8333';
    }
 }
 
