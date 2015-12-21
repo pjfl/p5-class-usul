cf2ef384d08b15916e01b7b46771b18a7a306dc0
pjfl <github@roxsoft.co.uk>
2015-12-21 02:41:28 +0000

    Added function interface to Log

--- lib/Class/Usul/Log.pm
+++ lib/Class/Usul/Log.pm
@@ -2,12 +2,11 @@ package Class::Usul::Log;
 
 use namespace::autoclean;
 
-use Class::Null;
 use Class::Usul::Constants qw( FALSE LOG_LEVELS NUL SPC TRUE );
-use Class::Usul::Functions qw( is_hashref is_member merge_attributes
+use Class::Usul::Functions qw( arg_list is_hashref is_member merge_attributes
                                untaint_identifier );
 use Class::Usul::Types     qw( Bool DataEncoding HashRef
-                               LoadableClass Logger Undef );
+                               LoadableClass Logger SimpleStr Undef );
 use Encode                 qw( encode );
 use File::Basename         qw( dirname );
 use File::DataClass::Types qw( Path );
@@ -15,11 +14,13 @@ use Scalar::Util           qw( blessed );
 use Sub::Install           qw( install_sub );
 use Moo;
 
-# Construction
-my $_build_log = sub {
-   my $self = shift; my $attr = $self->get_log_attributes;
+# Attribute constructors
+my $_build__log = sub {
+   return $_[ 0 ]->_log_class->new( %{ $_[ 0 ]->_log_attributes } );
+};
 
-   return $attr ? $self->_log_class->new( %{ $attr } ) : Class::Null->new;
+my $_build__log_class = sub {
+   return $_[ 0 ]->_logfile ? 'Log::Handler' : 'Class::Null';
 };
 
 # Private attributes
@@ -29,17 +30,21 @@ has '_debug_flag'     => is => 'ro',   isa => Bool, default => FALSE,
 has '_encoding'       => is => 'ro',   isa => DataEncoding | Undef,
    init_arg           => 'encoding';
 
-has '_log'            => is => 'lazy', isa => Logger, builder => $_build_log,
-   init_arg           => 'log';
+has '_log'            => is => 'lazy', isa => Logger,
+   builder            => $_build__log, init_arg => 'log';
 
-has '_log_attributes' => is => 'ro',   isa => HashRef, default => sub { {} },
-   init_arg           => 'log_attributes';
+has '_log_attributes' => is => 'ro',   isa => HashRef,
+   builder            => sub { {} },   init_arg => 'log_attributes';
 
 has '_log_class'      => is => 'lazy', isa => LoadableClass, coerce => TRUE,
-   default            => 'Log::Handler', init_arg => 'log_class';
+   builder            => $_build__log_class, init_arg => 'log_class';
 
 has '_logfile'        => is => 'ro',   isa => Path | Undef, coerce => TRUE,
    init_arg           => 'logfile';
+
+# Private class attributes
+my @arg_names = qw( level message options );
+my $loggers   = {};
 
 # Construction
 around 'BUILDARGS' => sub {
@@ -47,7 +52,7 @@ around 'BUILDARGS' => sub {
 
    my $builder = delete $attr->{builder} or return $attr;
    my $config  = $builder->can( 'config' ) ? $builder->config : {};
-   my $cfgattr = [ qw( encoding log_attributes log_class logfile ) ];
+   my $cfgattr = [ qw( appclass encoding log_attributes log_class logfile ) ];
 
    merge_attributes $attr, $builder, {}, [ 'debug' ];
    merge_attributes $attr, $config,  {}, $cfgattr;
@@ -55,47 +60,34 @@ around 'BUILDARGS' => sub {
    return $attr;
 };
 
-my $_add_methods = sub {
-   my ($class, @methods) = @_;
+sub BUILD {
+   my ($self, $attr) = @_;
 
-   for my $method (@methods) {
-      $class->can( $method )
-         or install_sub { into => $class, as => $method, code => sub {
-            my ($self, $text, $opts) = @_; $text or return FALSE;
-
-            $text = ucfirst "${text}"; chomp $text; $text .= "\n";
-
-            if (defined $opts) {
-               my $lead = ucfirst $opts->{leader} // NUL;
-               my $user = $opts->{user} ? '['.$opts->{user}->username.'] '
-                        :         $lead ? SPC
-                                        : NUL;
-
-               $text = "${lead}${user}${text}";
-            }
-
-            $self->_encoding and $text = encode( $self->_encoding, $text );
-            $self->_log->$method( $text );
-            return TRUE;
-         } };
-   }
+   exists $attr->{appclass}  and $loggers->{ $attr->{appclass} } = $self;
+   exists $loggers->{default} or $loggers->{default} = $self;
 
    return;
-};
-
-$_add_methods->( __PACKAGE__, LOG_LEVELS );
-
-# Public methods
-sub filehandle {
-   my $self = shift; $self->_log_class eq 'Log::Handler' or return;
-
-   return $self->_log->output( 'file-out' )->{fh};
 }
 
-sub get_log_attributes {
-   my $self = shift; my $attr = { %{ $self->_log_attributes } };
+sub import {
+   my $class  = shift;
+   my $params = { (is_hashref $_[ 0 ]) ? %{+ shift } : () };
+   my @wanted = @_;
+   my $target = caller;
+   my $subr   = $params->{as} // 'log';
 
-   $self->_log_class eq 'Log::Handler' or return $attr;
+   $wanted[ 0 ] and
+      install_sub { into => $target, as => $subr, code => sub (;@) {
+         return $loggers->{ $wanted[ 0 ] }->log( @_ );
+      } };
+
+   return;
+}
+
+around '_log_attributes' => sub {
+   my ($orig, $self) = @_; my $attr = $orig->( $self );
+
+   $self->_log_class ne 'Log::Handler' and return $attr;
 
    my $fattr   = $attr->{file} //= {};
    my $logfile = $fattr->{filename} // $self->_logfile;
@@ -107,18 +99,66 @@ sub get_log_attributes {
    $fattr->{maxlevel} = $self->_debug_flag ? 'debug'
                       : untaint_identifier $fattr->{maxlevel} // 'info';
    $fattr->{mode    } = untaint_identifier $fattr->{mode    } // 'append';
+
    return $attr;
+};
+
+# Private functions
+my $add_methods = sub {
+   my ($class, @methods) = @_;
+
+   for my $method (@methods) {
+      $class->can( $method ) or
+         install_sub { into => $class, as => $method, code => sub {
+            my ($self, $text, $opts) = @_; $text or return FALSE;
+
+            $text = ucfirst "${text}"; chomp $text; $text .= "\n";
+
+            if (defined $opts) {
+               my $lead = ucfirst $opts->{leader} // NUL;
+               my $tag  = $opts->{tag}
+                      // ($opts->{user} ? $opts->{user}->username : NUL);
+
+               $tag  = $tag ? "[${tag}] " : $lead ? SPC : NUL;
+               $text = "${lead}${tag}${text}";
+            }
+
+            $self->_encoding and $text = encode( $self->_encoding, $text );
+            $self->_log->$method( $text );
+            return TRUE;
+         } };
+   }
+
+   return;
+};
+
+$add_methods->( __PACKAGE__, LOG_LEVELS );
+
+my $inline_args = sub {
+   my $n = shift; return (map { $arg_names[ $_ ] => $_[ $_ ] } 0 .. $n - 1);
+};
+
+# Public methods
+sub filehandle {
+   my $self = shift; $self->_log_class ne 'Log::Handler' and return;
+
+   return $self->_log->output( 'file-out' )->{fh};
 }
 
 sub log {
-   my ($self, @args) = @_;
+   my ($self, @args) = @_; my $n = 0; $n++ while (defined $args[ $n ]);
 
-   my $args  = (is_hashref $args[ 0 ]) ? $args[ 0 ] : { @args };
-   my $level = $args->{level};
+   my $args  = ($n == 0              ) ? {}
+             : (is_hashref $args[ 0 ]) ? $args[ 0 ]
+             : ($n == 1              ) ? { $inline_args->( 2, 'info', @args ) }
+             : ($n == 2              ) ? { $inline_args->( 2, @args ) }
+             : ($n == 3              ) ? { $inline_args->( 3, @args ) }
+             :                           { @args };
 
-   is_member $level, LOG_LEVELS and return $self->$level( $args->{message} );
+   my $level = $args->{level}; is_member $level, LOG_LEVELS
+      and return $self->$level( $args->{message}, $args->{options} );
 
-   return;
+   return FALSE;
 }
 
 1;
@@ -126,6 +166,8 @@ sub log {
 __END__
 
 =pod
+
+=encoding utf-8
 
 =head1 Name
 
@@ -194,15 +236,14 @@ Path to the logfile
 
 Monkey with the constructors signature
 
+=head2 C<BUILD>
+
+Store the new object reference in a class attribute for later importation
+
 =head2 C<filehandle>
 
 Return the loggers file handle. This was added for L<IO::Async>, so that we
 can tell it not to close the log file handle when it forks a child process
-
-=head2 C<get_log_attributes>
-
-Returns the hash reference passed to the constructor of the log class. Returns
-C<undef> to indicate no logging, an instance of L<Class::Null> is used instead
 
 =head2 C<log>
 
--- t/16log.t
+++ t/16log.t
@@ -10,14 +10,19 @@ my $log  = Class::Usul::Log->new( encoding => 'UTF-8', logfile => $file );
 
 $log->debug( 'test' );
 unlike $file->all, qr{ \Q[DEBUG] Test\E }msx, 'Does not log debug level';
+
 $log->info ( 'test' );
 like $file->all, qr{ \Q[INFO] Test\E }msx, 'Log info level';
+
 $log->warn ( 'test' );
 like $file->all, qr{ \Q[WARNING] Test\E }msx, 'Log warning level';
+
 $log->error( 'test' );
 like $file->all, qr{ \Q[ERROR] Test\E }msx, 'Log error level';
+
 $log->alert( 'test' );
 like $file->all, qr{ \Q[ALERT] Test\E }msx, 'Log alert level';
+
 $log->fatal( 'test' );
 like $file->all, qr{ \Q[FATAL] Test\E }msx, 'Log fatal level';
 
@@ -26,6 +31,34 @@ $log = Class::Usul::Log->new
 
 $log->debug( 'test' );
 like $file->all, qr{ \Q[DEBUG] Test\E }msx, 'Log debug level';
+
+use Class::Usul::Log qw( default );
+
+my $r = log 'Function';
+
+like $file->all, qr{ \Q[INFO] Function\E }msx, 'Imports function default level';
+is $r, 1, 'Log function returns true';
+
+log 'warn', 'Watchit';
+like $file->all, qr{ \Q[WARNING] Watchit\E }msx, 'Level and message';
+
+log 'error', 'Failed', { leader => 'Lead', tag => 'ID' };
+like $file->all, qr{ \Q[ERROR] Lead[ID] Failed\E }msx,
+   'Level message and options';
+
+log { level => 'fatal', message => 'Brown Bread' };
+like $file->all, qr{ \Q[FATAL] Brown Bread\E }msx, 'From hash reference';
+
+log level => 'alert', message => 'Wake Up';
+like $file->all, qr{ \Q[ALERT] Wake Up\E }msx, 'From list of keys and values';
+
+my $size = $file->stat->{size}; $r = log;
+
+is $file->stat->{size}, $size, 'Logs nothing with no args';
+is $r, 0, 'Returns false when not logging';
+
+log 'nochance', 'testing';
+is $r, 0, 'Non existant level returns false';
 
 $file->exists and $file->unlink;
 done_testing;
